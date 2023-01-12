605a2e6caa1ba8ee60b8cfb228a74d24d92bba89
Emil Åkerman <emil@Emils-MacBook-Air.local>
2023-01-12 13:06:28 +0100

    removed items now added to recently deleted

--- untitledtvproject/ContentView.swift
+++ untitledtvproject/ContentView.swift
@@ -21,6 +21,9 @@ struct ContentView: View {
                             RowView(show: show)
                         }
                     }
+                    .onDelete() { indexSet in
+                        delete(indexSet: indexSet)
+                    }
                 }
                 Section(header: Text("Watching")) {
                     ForEach(showList.watching) { show in
@@ -28,12 +31,18 @@ struct ContentView: View {
                             RowView(show: show)
                         }
                     }
+                    .onDelete() { indexSet in
+                        delete(indexSet: indexSet)
+                    }
                 }
             Section(header: Text("Completed")) {
                     ForEach(showList.completed) { show in
                         NavigationLink(destination: ShowEntryView(show: show, title: show.title, seasons: show.seasons, episodes: show.episodes)) {
                             RowView(show: show)
                         }
+                    }
+                    .onDelete() { indexSet in
+                        delete(indexSet: indexSet)
                     }
                 }
             Section(header: Text("Dropped")) {
@@ -46,13 +55,23 @@ struct ContentView: View {
                             delete(indexSet: indexSet)
                         }
                     }
+                Section(header: Text("Recently deleted")) {
+                        ForEach(showList.recentlyDeleted) { show in
+                            NavigationLink(destination: ShowEntryView(show: show, title: show.title, seasons: show.seasons, episodes: show.episodes)) {
+                                RowView(show: show)
+                            }
+                        }
+                    }
                 }
             }
         }
     }
     func delete(indexSet: IndexSet) { //move to ShowEntry struct, when its deleted from the "shows" list, then it should be removed from the other lists too, but added to "deleted" list
-        showList.dropped.remove(atOffsets: indexSet)
-        //showList.shows.remove(atOffsets: indexSet)
+        for index in indexSet {
+            let item = showList.dropped[index]
+            showList.dropped.remove(atOffsets: indexSet)
+            showList.recentlyDeleted.append(item)
+        }
     }
     func goToInfoPage() {
 
